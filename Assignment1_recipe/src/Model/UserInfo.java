2d19b0137030dd4b0d0a39282144a9967b9d3a40
emily <wang.shig@northeastern.edu>
2023-01-30 18:45:05 -0500

    add assignment1 branch and push code for basic functionality

---
+++ Assignment1_recipe/src/Model/UserInfo.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author emi
+ */
+public class UserInfo {
+    String firstName; //no special characters or numbers, only blank and letters
+    String lastName;
+    String userName; //do not allow update
+    String email; //######@####.###
+    long phone; //10 numbers, ideally String?
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+}
